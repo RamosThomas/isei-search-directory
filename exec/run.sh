165a8b9712dd7cf5bb59fec52ac1f1a68139003d
thom8047 <thom8047@bears.unco.edu>
2022-02-27 13:52:53 -0700

    Add browserify & express - Browserify to bundle and serve up the javascript files - Express to serve the html file - Added the needed npm scripts to run things properly

---
+++ exec/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+npm run compile && npm run browserify-build && node server.js
---
+++ project/ts/main.ts
@@ -0,0 +1,17 @@
+import { getApplicableDataAsync } from "./getApplicableDataAsync";
+
+const form = document.getElementById("form");
+
+// form?.addEventListener("keydown", (event: Event | any) => {
+//   const target: any = event.target;
+//   console.log(target?.value);
+//   return;
+// });
+
+// Get ID's and then the values off them. Log them on submit.
+
+form?.addEventListener("submit", (event: Event) => {
+  event.preventDefault();
+
+  //   console.log();
+});
